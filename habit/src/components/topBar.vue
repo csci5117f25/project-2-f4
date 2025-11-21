9f49f5f6bc8bc71ecc4fb2445469e7ee13b63e1d
Calvin Chau <calvin.chau0@gmail.com>
2025-11-21 11:30:46 -0600

    reworked navbar

--- habit/src/components/topBar.vue
+++ habit/src/components/topBar.vue
@@ -1,37 +1,52 @@
 <script setup>
+import { useCurrentUser } from "vuefire";
+import { useRouter, useRoute } from "vue-router";
+import { onMounted, watch } from "vue";
+import { signIn, logOut } from "../useAuth";
 
-import { useCurrentUser } from 'vuefire'
-import { useRouter, useRoute } from 'vue-router'
-import { onMounted, watch } from 'vue'
-import { signIn , logOut} from '../useAuth'
-
-const user = useCurrentUser()
-const router = useRouter()
-const route = useRoute()
+const user = useCurrentUser();
+const router = useRouter();
+const route = useRoute();
 
 async function login() {
-  await signIn().catch(err => console.log(err))
-  if (user.value) router.replace('/tracker')
+  await signIn().catch((err) => console.log(err));
+  if (user.value) router.replace("/tracker");
 }
 
 async function logout() {
-  await logOut().catch(err => console.log(err))
-  router.replace('/')
+  await logOut().catch((err) => console.log(err));
+  router.replace("/");
 }
 
 function maybeRedirect() {
-  if (user.value && route.path === '/') {
-    router.replace('/tracker')
+  if (user.value && route.path === "/") {
+    router.replace("/tracker");
   }
 }
 
-onMounted(maybeRedirect)
-watch([user, () => route.path], maybeRedirect)
+onMounted(maybeRedirect);
+watch([user, () => route.path], maybeRedirect);
 </script>
 
-<template lang="pug">
-header
-  div(style="display:flex; justify-content:flex-end; width:100%")
-    button(v-if="!user" @click="login") Login
-    button(v-else @click="logout") Logout
+<template>
+  <div class="navbar bg-base-100 shadow-sm fixed top-0 left-0 right-0 z-50 w-full">
+    <div class="navbar-start pl-8">
+      <RouterLink to="/" class="btn btn-ghost text-xl">Habit Tracker</RouterLink>
+    </div>
+
+    <div class="flex items-center gap-6">
+      <RouterLink to="/" class="btn btn-ghost text-base">Home</RouterLink>
+      <RouterLink to="/tracker" class="btn btn-ghost text-base">Tracker</RouterLink>
+      <RouterLink to="/past-journals" class="btn btn-ghost text-base">View Past Journals</RouterLink>
+    </div>
+
+    <div class="navbar-end gap-2 pr-8">
+      <button v-if="!user" @click="login()" class="btn btn-primary">Login</button>
+      <div v-else class="flex gap-6 items-center">
+        <p>Welcome, {{ user.displayName }}!</p>
+
+        <button @click="logout()" class="btn btn-secondary">Logout</button>
+      </div>
+    </div>
+  </div>
 </template>
--- habit/src/views/PastJournals.vue
+++ habit/src/views/PastJournals.vue
@@ -1,13 +1,6 @@
 <template>
 
 <h2 class="text-3xl underline">Past Journals</h2>
-<button class="btn btn-xs primary">Xsmall</button>
-<button class="btn btn-sm">Small</button>
-<button class="btn">Medium</button>
-<button class="btn btn-lg">Large</button>
-<button class="btn btn-xl">Xlarge</button>
-
-
 
 
 
